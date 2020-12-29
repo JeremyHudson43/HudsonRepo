8fb15b7726cbbfbcdb62a9c9c54d37cd9e8ec4ce
JeremyHudson43 <jer4349@gmail.com>
2020-12-29 17:14:50 -0500

    Add files via upload

---
+++ Sample Company Database/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Configuration for database connection
+ *
+ */
+
+$host       = "fdb18.biz.nf";
+$username   = "3407887_company";
+$password   = "BA^3K}FD2a;Ve(9#";
+$dbname     = "3407887_company";
+$dsn        = "mysql:host=$host;dbname=$dbname";
+$options    = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+);
