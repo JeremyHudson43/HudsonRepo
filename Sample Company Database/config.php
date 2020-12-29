61b96cc7131ae59fbfdaf1020948be47b34a35d4
JeremyHudson43 <jer4349@gmail.com>
2020-12-29 17:24:09 -0500

    Update config.php

--- Sample Company Database/config.php
+++ Sample Company Database/config.php
@@ -5,11 +5,11 @@
  *
  */
 
-$host       = "fdb18.biz.nf";
-$username   = "3407887_company";
-$password   = "BA^3K}FD2a;Ve(9#";
-$dbname     = "3407887_company";
-$dsn        = "mysql:host=$host;dbname=$dbname";
+$host       = "";
+$username   = "";
+$password   = "";
+$dbname     = "";
+$dsn        = "";
 $options    = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 );
